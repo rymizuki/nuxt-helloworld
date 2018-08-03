90974dd74a9dd55685839be415158a17422081ea
mizuki_r <ry.mizuki@gmail.com>
2018-08-04 05:58:40 +0900

    ローカルで開発できるように調整

--- app/src/server.js
+++ app/src/server.js
@@ -1,23 +1,19 @@
-import express from 'express'
-import { Nuxt } from 'nuxt'
 import awsServerlessExpress from 'aws-serverless-express'
 import awsServerlessExpressMiddleware from 'aws-serverless-express/middleware'
-import { Server } from 'tls';
-import api from './api'
+import app from './app'
 
-const config = require('../nuxt.config')
-config.dev = false
-config.mode = 'universal'
-const nuxt = new Nuxt(config)
-
-const app = express()
-
-app.use('/api', api)
-
-app.use(nuxt.render)
-app.use(awsServerlessExpressMiddleware.eventContext)
-
-const server = awsServerlessExpress.createServer(app)
+let server;
+if (__DEV__) {
+  const port = process.env.PORT || '3000'
+  const host = process.env.HOST || '0.0.0.0'
+  app.listen(port, host, () => {
+    console.log('> in development server')
+    console.log(`> listening on http://${ host }:${ port }`)
+  })
+} else {
+  app.use(awsServerlessExpressMiddleware.eventContext)
+  server = awsServerlessExpress.createServer(app)
+}
 
 export function render(event, context) {
   awsServerlessExpress.proxy(server, event, context)
